0c1c1001caa2d7985d0a581ea8a78c661bfc06b9
jiahoe <mrjiahoe@hotmail.com>
2021-12-20 20:25:09 +0800

    add dark and light mode toggle

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Search } from "./Search";
 
 export const Navbar = ({ darkTheme, setDarkTheme }) => {
 	return (
@@ -10,8 +11,15 @@ export const Navbar = ({ darkTheme, setDarkTheme }) => {
 						Google2.0 🔎
 					</p>
 				</Link>
-				<button type="button" onClick={() => setDarkTheme(!darkTheme)}></button>
+				<button
+					type="button"
+					onClick={() => setDarkTheme(!darkTheme)}
+					className="text-xl dark:bg-gray-50 dark:text-gray-900 bg-white border rounded-full px-2 py-1 hover:shadow-lg"
+				>
+					{darkTheme ? "💡 Light" : "🌙 Dark"}
+				</button>
 			</div>
+			<Search />
 		</div>
 	);
 };
