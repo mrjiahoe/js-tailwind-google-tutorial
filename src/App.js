d2a9285f0ad2d5a3d4430cc88edcbb7a3b12932e
jiahoe <mrjiahoe@hotmail.com>
2021-12-21 13:44:46 +0800

    change folder name to support rrd v6

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 import { Navbar } from "./components/Navbar";
 import { Footer } from "./components/Footer";
-import { Routes } from "./components/Routes";
+import { RoutesTo } from "./components/RoutesTo";
 
 const App = () => {
 	const [darkTheme, setDarkTheme] = useState(false);
@@ -10,7 +10,7 @@ const App = () => {
 		<div className={darkTheme ? "dark" : ""}>
 			<div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
 				<Navbar darkTheme={darkTheme} setDarkTheme={setDarkTheme} />
-				<Routes />
+				<RoutesTo />
 				<Footer />
 			</div>
 		</div>
